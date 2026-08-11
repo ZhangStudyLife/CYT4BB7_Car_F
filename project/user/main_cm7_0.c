02dff66cddf99dad89d549fd67bd0146ad082d77
ZhangStudyLife <174326754+ZhangStudyLife@users.noreply.github.com>
2026-08-11 12:32:37 +0800

    修改发送的日志

--- project/user/main_cm7_0.c
+++ project/user/main_cm7_0.c
@@ -20,35 +20,34 @@ int main(void)
         car_loop_poll();
         car_mode2_get_diag(&mode2_diag);
 
-        wifi_justfloat(g_air_std_ch0,
-                        g_air_std_ch1,
-                        g_air_std_ch4,
-                        g_air_std_ch5,
-                        g_air_std_ch6,
-                        car_mode_get(),
-                        car_mode_is_control_enabled(),
-                        car_safety_is_output_allowed(),
-                        car_safety_get_fault(),
-                        g_euler.roll,
-                        g_euler.pitch,
-                        g_euler.yaw,
-                        g_car_gyroz_feedback_dps,
-                        mode2_diag.yaw_target_deg,
-                        mode2_diag.yaw_error_deg,
-                        mode2_diag.gyroz_target_dps,
-                        mode2_diag.gyroz_output,
-                        mode2_diag.large_turn_state,
-                        mode2_diag.large_turn_rearm_required,
-                        mode2_diag.speed_brake_active,
-                        car_speed_encoder_cnt_to_mps(g_car_base_speed_command),
-                        car_speed_encoder_cnt_to_mps(g_car_base_speed_target),
-                        car_speed_encoder_cnt_to_mps(Left_Target_Speed),
-                        car_speed_encoder_cnt_to_mps(Right_Target_Speed),
-                        car_speed_encoder_cnt_to_mps(g_car_speed_left_filtered),
-                        car_speed_encoder_cnt_to_mps(g_car_speed_right_filtered),
-                        encoder_get_left_count(),
-                        encoder_get_right_count(),
-                        g_car_speed_left_motor_output,
-                        g_car_speed_right_motor_output);
+        wifi_justfloat(car_mode_get(),                                      /* I1  car mode */
+                       car_mode_is_control_enabled(),                       /* I2  control enabled */
+                       car_safety_is_output_allowed(),                      /* I3  output allowed */
+                       car_safety_get_fault(),                              /* I4  safety fault */
+                       air_comm_car_is_run_data_fresh(),                    /* I5  air data fresh */
+                       g_air_state,                                         /* I6  aircraft state */
+                       g_air_std_ch0, g_air_std_ch1, g_air_std_ch2,          /* I7-I9   CRSF ch0-ch2 */
+                       g_air_std_ch3, g_air_std_ch4, g_air_std_ch5,          /* I10-I12 CRSF ch3-ch5 */
+                       g_air_std_ch6, g_air_std_ch7, g_air_std_ch8,          /* I13-I15 CRSF ch6-ch8 */
+                       g_air_yaw_angle_target_deg,                           /* I16 aircraft yaw target, deg */
+                       g_air_sync_time_ms,                                  /* I17 aircraft time, ms */
+                       g_air_car_plan_valid,                                /* I18 car plan valid */
+                       g_air_car_plan_strafe_mps,                           /* I19 plan strafe, m/s */
+                       g_air_car_plan_forward_mps,                          /* I20 plan forward, m/s */
+                       g_air_beacon_lost_flag,                              /* I21 beacon lost */
+                       tick_1000us_cnt,                                     /* I22 car air-data time, ms */
+                       g_euler.roll, g_euler.pitch, g_euler.yaw,             /* I23-I25 car Euler, deg */
+                       g_imufilter_1000hz.accx,                              /* I26 car IMU acc X, g */
+                       g_imufilter_1000hz.accy,                              /* I27 car IMU acc Y, g */
+                       g_imufilter_1000hz.accz,                              /* I28 car IMU acc Z, g */
+                       g_imufilter_1000hz.gyroz,                             /* I29 car horizontal yaw rate, deg/s */
+                       mode2_diag.yaw_target_deg,                            /* I30 car yaw target, deg */
+                       g_car_yaw_feedback_deg,                              /* I31 car yaw feedback, deg */
+                       car_speed_encoder_cnt_to_mps(g_car_base_speed_command), /* I32 raw speed command, m/s */
+                       car_speed_encoder_cnt_to_mps(g_car_base_speed_target),  /* I33 speed-loop target, m/s */
+                       car_speed_encoder_cnt_to_mps(0.5f *
+                           (g_car_speed_left_filtered + g_car_speed_right_filtered)), /* I34 speed feedback, m/s */
+                       mode2_diag.gyroz_target_dps,                          /* I35 yaw-rate target, deg/s */
+                       g_car_gyroz_feedback_dps);                            /* I36 yaw-rate feedback, deg/s */
     }
 }
